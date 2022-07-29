7c6a706940e7748c33ace2efef3d8473533ce9f5
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-07-28 21:09:07 -0300

    chore: making about and skills components

--- src/pages/Landpage.js
+++ src/pages/Landpage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Home from "../components/Home";
+import About from "../components/About";
+import Skills from "../components/Skills";
 
 function Landpage() {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
+      <Skills />
       <section>
         PROJECTS
       </section>
