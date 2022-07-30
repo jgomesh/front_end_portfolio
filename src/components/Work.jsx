f2d2616870f468ade8d641bb91ec4743d62bf608
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-07-30 17:46:58 -0300

    fix: scroll to top

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,12 +5,11 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 function Work() {
   return (
     <div name="work" className="w-full py-[100px]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center ">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">Work</p>
           <p className="py-6">Here are my projects:</p>
         </div>
-
         {/* Container */}
         <div
           className="container-image grid sm:grid-cols-2 md:grid-cols-3 gap-4">
@@ -71,24 +70,6 @@ function Work() {
             </div>
           </div>
           
-          {/* Grid Item */}
-          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
-            
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Trybe Wallet
-              </span>
-              <div className="pt-8 text-center">
-                <a href='/'>
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
-                </a>
-                <a href='/'>
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
           <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div2">
             
             {/* Hover Effects */}
@@ -97,16 +78,16 @@ function Work() {
                 Trybe Tunes
               </span>
               <div className="pt-8 text-center">
-                <a href='/'>
+                <a href="https://jgomesh.github.io/trybe-tunes/" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                 </a>
-                <a href='/'>
+                <a href="https://github.com/jgomesh/trybe-tunes" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
             </div>
           </div>
-          
+          <div></div>
           {/* Grid Item */}
           <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div4">
             
@@ -116,10 +97,10 @@ function Work() {
                 Simple Landpage
               </span>
               <div className="pt-8 text-center">
-                <a href='/'>
+                <a href="https://jgomesh.github.io/site-barbearia/" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                 </a>
-                <a href='/'>
+                <a href="https://github.com/jgomesh/site-barbearia" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Work from "../components/Work";
 import Skills from "../components/Skills";
 
 function Project({ history }) {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, []);
+
   return (
     <>
     <div className="space-bg">
       <Navbar history={ history }/>
-      <Work />
+        <Work />
       <Skills />
     </div>
     </>
